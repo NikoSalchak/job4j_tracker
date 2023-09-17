3f4b079e7f84dc4faeeb349183607ce028a66873
NikoSalchak <nikosalchak@gmail.com>
2023-09-17 18:24:56 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error first = new Error();
+        first.printInfo();
+        Error second = new Error(true, 5, "unknown error");
+        second.printInfo();
+    }
+}
