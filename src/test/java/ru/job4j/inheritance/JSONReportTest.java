575c22eb88e246b325addb374ca33588c13a799a
NikoSalchak <nikosalchak@gmail.com>
2023-09-22 20:49:21 +0700

    4. Переопределение метода [#174890 #447039]

---
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.inheritance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JSONReportTest {
+
+    @Test
+    public void whenTestGenerateMethod() {
+        String ln = System.lineSeparator();
+        String expected = "{" + ln
+                + "\t\"name\" : \"Report's name\"," + ln
+                + "\t\"body\" : \"Report's body\"" + ln
+                + "}";
+        String name = "Report's name";
+        String body = "Report's body";
+        String result = new JSONReport().generate(name, body);
+        assertThat(result).isEqualTo(expected);
+    }
+}
