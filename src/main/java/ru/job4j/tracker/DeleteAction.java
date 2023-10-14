5ada05dbed40f646c4ba782d400e5fc6cc9dd5d7
NikoSalchak <nikosalchak@gmail.com>
2023-10-14 19:08:37 +0700

    12.1 Зависимость от System.out [#33568 #452426]

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -14,11 +14,11 @@ public class DeleteAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("==== Delete the item ===");
+        out.println("==== Delete the item ===");
         int id = input.askInt("Enter id: ");
         Item item = tracker.findById(id);
         tracker.delete(id);
-        System.out.println(item != null ? "The item deleted successfully." : "Item delete error.");
+        out.println(item != null ? "The item deleted successfully." : "Item delete error.");
         return true;
     }
 }
