3d3dc55f46f461210a9b286c3fda90eb278827cc
NikoSalchak <nikosalchak@gmail.com>
2023-10-15 16:56:02 +0700

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index] == key) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("the index wasn't found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+        String[] value = {"first", "second", "third", "fourth"};
+        String key = "fifth";
+        System.out.println(indexOf(value, key));
+        } catch (ElementNotFoundException el) {
+            el.printStackTrace();
+        }
+    }
+}
