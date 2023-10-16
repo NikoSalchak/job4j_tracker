6f3389bb932111c3ec11f103883a7a5176bf47e7
NikoSalchak <nikosalchak@gmail.com>
2023-10-16 21:55:50 +0700

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -178,4 +178,26 @@ class StartUITest {
                         + "The program shut down" + ln
         );
     }
+
+    @Test
+    void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"3", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+          new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + ln
+                + "0. Shut down the program" + ln
+                + "wrong input, you can choose: 0 .. 0" + ln
+                + "Menu:" + ln
+                + "0. Shut down the program" + ln
+                + "The program shut down" + ln
+        );
+    }
 }
