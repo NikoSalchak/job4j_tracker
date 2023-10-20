03e2376c67f7bee80eff7b8b8d85ca71d059a96f
NikoSalchak <nikosalchak@gmail.com>
2023-10-20 15:06:50 +0700

    4. Принципы раннего возврата и охранных выражений [#504875 #453773]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -17,15 +17,19 @@ public class PasswordValidator {
         for (char symbol : password.toCharArray()) {
             if (Character.isUpperCase(symbol)) {
                 hasUpCase = true;
+                continue;
             }
             if (Character.isLowerCase(symbol)) {
                 hasLowCase = true;
+                continue;
             }
             if (Character.isDigit(symbol)) {
                 hasDigit = true;
+                continue;
             }
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
+                break;
             }
         }
         if (!hasUpCase) {
