4296631a2a10d199fbd4104652a7823d3955cfd5
NikoSalchak <nikosalchak@gmail.com>
2023-11-15 19:38:35 +0700

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,44 +1,42 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final List<Item> items = new ArrayList<>(100);
     private int ids = 1;
-    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+    public List<Item> findAll() {
+        return items;
     }
 
-    public Item[] findByName(String key) {
-        int counter = 0;
-        Item[] rsl = new Item[size];
-        for (int i = 0; i < size; i++) {
-            Item temp = items[i];
-            if (temp.getName().equals(key)) {
-                rsl[counter] = items[i];
-                counter++;
+    public List<Item> findByName(String key) {
+        List<Item> rsl = new ArrayList<>();
+        for (int index = 0; index < items.size(); index++) {
+            Item item = items.get(index);
+            if (item.getName().contains(key)) {
+                rsl.add(item);
             }
         }
-        return Arrays.copyOf(rsl, counter);
+        return rsl;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
             }
@@ -51,7 +49,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
-            items[index] = item;
+            items.set(index, item);
         }
         return rsl;
     }
@@ -59,9 +57,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
+            items.remove(index);
         }
     }
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,9 +2,14 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
+
     @Test
     void whenCreateItem() {
         Output out = new StubOutput();
@@ -12,12 +17,13 @@ class StartUITest {
                 new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
-        UserAction[] actions = {
+        List<Item> items = tracker.findAll();
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new CreateAction(out),
-                new ExitAction(out)
-        };
+                new ExitAction(out))
+        );
         new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+        assertThat(items.get(0).getName()).contains(new Item("Item name").getName());
     }
 
     @Test
@@ -29,10 +35,9 @@ class StartUITest {
         Input in = new StubInput(
                 new String[]{"0", String.valueOf(item.getId()), "New item name", "1"}
         );
-        UserAction[] actions = {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new ReplaceAction(out),
-                new ExitAction(out)
-        };
+                new ExitAction(out)));
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
@@ -45,10 +50,9 @@ class StartUITest {
         Input in = new StubInput(
                 new String[]{"0", String.valueOf(item.getId()), "1"}
         );
-        UserAction[] actions = {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new DeleteAction(out),
-                new ExitAction(out)
-        };
+                new ExitAction(out)));
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
     }
@@ -60,9 +64,9 @@ class StartUITest {
                 new String[]{"0"}
         );
         Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new ExitAction(out)
-        };
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
+                new ExitAction(out))
+        );
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString()).isEqualTo(
                 "Menu:" + System.lineSeparator()
@@ -77,24 +81,23 @@ class StartUITest {
         Item one = tracker.add(new Item("test"));
         String replaceName = "New Test Name";
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+                new String[]{"0", String.valueOf(one.getId()), replaceName, "1"}
         );
-        UserAction[] actions = new UserAction[] {
-          new ReplaceAction(out),
-          new ExitAction(out),
-        };
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
+                new ReplaceAction(out),
+                new ExitAction(out)));
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
                 "Menu:" + ln
-                + "0. Change the item" + ln
-                + "1. Shut down the program" + ln
-                +  "==== Change the item ===" + ln
-                + "The item changed successfully." + ln
-                + "Menu:" + ln
-                + "0. Change the item" + ln
-                + "1. Shut down the program" + ln
-                + "The program shut down" + ln
+                        + "0. Change the item" + ln
+                        + "1. Shut down the program" + ln
+                        + "==== Change the item ===" + ln
+                        + "The item changed successfully." + ln
+                        + "Menu:" + ln
+                        + "0. Change the item" + ln
+                        + "1. Shut down the program" + ln
+                        + "The program shut down" + ln
         );
     }
 
@@ -104,19 +107,19 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test"));
         Input in = new StubInput(
-                new String[] {"0", "1"}
+                new String[]{"0", "1"}
         );
-        UserAction[] actions = new UserAction[] {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new FindAllAction(out),
-                new ExitAction(out),
-        };
+                new ExitAction(out))
+        );
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
                 "Menu:" + ln
                         + "0. Show All the items" + ln
                         + "1. Shut down the program" + ln
-                        +  "=== Show all the items ===" + ln
+                        + "=== Show all the items ===" + ln
                         + one + ln
                         + "Menu:" + ln
                         + "0. Show All the items" + ln
@@ -131,12 +134,11 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test"));
         Input in = new StubInput(
-                new String[] {"0", one.getName(), "1"}
+                new String[]{"0", one.getName(), "1"}
         );
-        UserAction[] actions = new UserAction[] {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new FindByNameAction(out),
-                new ExitAction(out),
-        };
+                new ExitAction(out)));
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
@@ -158,12 +160,12 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), "1"}
+                new String[]{"0", String.valueOf(one.getId()), "1"}
         );
-        UserAction[] actions = new UserAction[] {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new FindByIdAction(out),
-                new ExitAction(out),
-        };
+                new ExitAction(out))
+        );
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
@@ -183,21 +185,21 @@ class StartUITest {
     void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"3", "0"}
+                new String[]{"3", "0"}
         );
         Tracker tracker = new Tracker();
-        UserAction[] actions = new UserAction[]{
-          new ExitAction(out)
-        };
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
+                new ExitAction(out))
+        );
         new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
                 "Menu:" + ln
-                + "0. Shut down the program" + ln
-                + "wrong input, you can choose: 0 .. 0" + ln
-                + "Menu:" + ln
-                + "0. Shut down the program" + ln
-                + "The program shut down" + ln
+                        + "0. Shut down the program" + ln
+                        + "wrong input, you can choose: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Shut down the program" + ln
+                        + "The program shut down" + ln
         );
     }
 }
