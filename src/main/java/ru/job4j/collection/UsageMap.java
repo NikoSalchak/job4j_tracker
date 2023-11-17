b8e0b7d18fdad3a67173bc799f76d0490017ff73
NikoSalchak <nikosalchak@gmail.com>
2023-11-17 22:33:11 +0700

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
-        HashMap<String, String>map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("nikosalchak@gmail.com", "Niko Salchak Mergenovich");
         for (String key: map.keySet()) {
